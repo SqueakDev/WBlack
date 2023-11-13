8fcf638d327bce75f6578f1a798608b3c9cce4be
Squeak Dev <150526837+SqueakDev@users.noreply.github.com>
2023-11-13 16:45:24 +0700

    Create db.py

---
+++ Database/db.py
@@ -0,0 +1,14 @@
+import sqlite3
+import json
+import random
+
+Db='./Db/player.sqlite'
+
+def __init__(self,player,Token,UserID,money):
+	DB=[
+	['NamePlayer'],
+	['UserID'],
+	["Token"]
+	['VIP']
+	['Money']
+  ]

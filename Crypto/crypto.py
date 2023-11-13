486893dbc56c79da376bd3a9b9b9dc477ca4d908
Squeak Dev <150526837+SqueakDev@users.noreply.github.com>
2023-11-13 16:44:21 +0700

    Create crypto.py

---
+++ Crypto/crypto.py
@@ -0,0 +1,26 @@
+import time
+import socket
+import logging
+import tinydb
+import json
+print(f"crypto install...")
+
+for i in range(101):
+	print(f"Установка:{i}%")
+	time.sleep(3)
+if tinydb=="Crypto":
+		tinydb.check()
+elif logging=='crypto2':
+			logging.run()
+			def _(*args):
+				for  args in range(3):
+					args.open()
+					key=""
+					key.running
+					crypto="./json/crypto.json"
+					json.load(crypto)
+					mongoDB=True
+					mongo=mongoDB
+					if __init__=='__main__':
+						Server=Server("89.0.0.2",7878)
+						Server.start()

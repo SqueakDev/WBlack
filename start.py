c64dd942488cc155f5447ec7694ebebff788e260
Squeak Dev <150526837+SqueakDev@users.noreply.github.com>
2023-11-13 16:26:46 +0700

    Add files via upload

---
+++ start.py
@@ -0,0 +1,61 @@
+import os
+import logging
+import socket
+import json
+import sqlite3
+import csv
+import sqlite3 as sql
+import random
+import time
+import byte
+print("Wblack runnig....")
+input()
+cv2="OpenAI"
+
+def _(*args):
+	for args in range(0):
+		args.packet()
+		cv2.open()
+		object="Car"
+		x=30
+		y=30
+		lines=10
+		object.writeLines(x,y,lines)
+		if x==y:
+			object.update()	
+			if x>y:
+				object.off()
+			else:
+					return object		
+					def __init__(self,object,cv2):
+						cv2.run()
+						cv2.self=self
+						cv2.player=player['NamePlayer']
+						object=people
+						object.off()
+						class MyAPP:
+								self.app=logging
+								self.ip=ip
+								self.port=port
+								self.MyAPP="Wblack"
+		elif Server.running==True:
+									Server=Server('89.0.0.2',7878)
+									if __init__=='__main__' :
+										Main()
+										return time
+										csv1="./csv/theme.csv"
+										csv.load(csv1)	
+										file.open(csv1 )
+										file.runnig()
+										os.path()
+										sqlite=db
+										from Database.Db import db 
+										sqlite.open(db)
+										sqlite.add()
+										sqlite.save()
+										Account.add()
+										Account.save()
+										json1="./json/acc.json"
+										json.load(json1)
+										json2="./json/byte.json"
+										json.load(json2)
